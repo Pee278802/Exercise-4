97ccc3c8395a361f29515207a4ff0183c95a012d
raymo <raymo@DESKTOP-VIK2O2O>
2021-04-21 09:21:43 +0800

    Exercise4

--- Exercise4/src/Main.java
+++ Exercise4/src/Main.java
@@ -3,25 +3,41 @@ public class Main {
 
 	public static void main(String[] args) {
 		iPhone iPhone = new iPhone();
-		System.out.println(iPhone.getWeight());
-		System.out.println("Color: " + iPhone.getColor());
+		System.out.println("Length: " + iPhone.getLength());
+		System.out.println("Width:" + iPhone.getWidth());
+		System.out.println("Height: " + iPhone.getHeight());
+		System.out.println("Weight: " + iPhone.getWeight());
+		System.out.println("Color:v" + iPhone.getColor());
 		System.out.println("Processor: " + iPhone.getProcessor());
-		System.out.println("Storage: " + iPhone.getStorage() + "GB");
-		System.out.println("Price: " + iPhone.getPrice());
+		System.out.println("Storage: " + iPhone.getStorage()+ "GB");
+		System.out.println("Price: RM" + iPhone.getPrice());
 		System.out.println("Quantity: " + iPhone.getQuantity());
-		System.out.println("Total Price: " + iPhone.totalPrice);
-
+		System.out.println("Total Price: " + iPhone.getTotalPrice());
+		System.out.println("On/Off: " + iPhone.getOnOff());
+		System.out.println("Wi-Fi: " + iPhone.getWifi());
+		System.out.println("Password: " + iPhone.getPassword());
+		System.out.println("Bluetooth: " + iPhone.getBluetooth());
+		System.out.println("Brightness: " + iPhone.getBrightness());
 		System.out.println();
 		
 		Samsung Samsung = new Samsung();
 		System.out.println("------Samsung------");
-		System.out.println(Samsung.getWeight());
-		System.out.println("Color: " + Samsung.getColor());
+		System.out.println("Length: " + Samsung.getLength());
+		System.out.println("Width:" + Samsung.getWidth());
+		System.out.println("Height: " + Samsung.getHeight());
+		System.out.println("Weight: " + Samsung.getWeight());
+		System.out.println("Color:v" + Samsung.getColor());
 		System.out.println("Processor: " + Samsung.getProcessor());
-		System.out.println("Storage: " + Samsung.getStorage() + "GB");
-		System.out.println("Price: " + Samsung.getPrice());
+		System.out.println("Storage: " + Samsung.getStorage()+ "GB");
+		System.out.println("Price: RM" + Samsung.getPrice());
 		System.out.println("Quantity: " + Samsung.getQuantity());
-		System.out.println("Total Price: " + Samsung.totalPrice);
+		System.out.println("Total Price: " + Samsung.getTotalPrice());
+		System.out.println("On/Off: " + Samsung.getOnOff());
+		System.out.println("Wi-Fi: " + Samsung.getWifi());
+		System.out.println("Password: " + Samsung.getPassword());
+		System.out.println("Bluetooth: " + Samsung.getBluetooth());
+		System.out.println("Brightness: " + Samsung.getBrightness());
+		System.out.println();
 
 
 	}
